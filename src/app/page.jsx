94d29b663cc09dd6fcf70d649cd3f7b1dbad4552
Antonio obaid <abo.george11@hotmail.com>
2024-10-19 20:20:41 +0200

    resopnsiv homePage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ function Home() {
   const [boende, setBoende] = useState([]);
   const [imageUrls, setImageUrls] = useState([]);
   const [mainImageUrl, setMainImageUrl] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");  // Nytt state för felmeddelande
+  const [errorMessage, setErrorMessage] = useState(""); 
   const router = useRouter();
 
   useEffect(() => {
@@ -68,134 +68,124 @@ function Home() {
   };
 
   return (
-    <div>
-      <section className="flex justify-between items-center mt-10 mx-24">
-        <div className="max-w-80 bg-white p-4 border border-green-600 rounded-md">
-          <h1 className="text-3xl font-bold text-black mb-4">Hitta boenden på Airbnb</h1>
-          <p className="mb-6 text-gray-700">Upptäck hela boenden och rum som är perfekta för alla resor.</p>
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <div>
-              <label htmlFor="location" className="block text-sm font-medium text-gray-700">
-                PLATS
-              </label>
-              <select
-                id="location"
-                value={location}
-                onChange={(e) => setLocation(e.target.value)}
-                className="mt-1 block w-full border-gray-300 text-black rounded-md shadow-sm"
-              >
-                <option value="">Välj stad</option>
-                <option value="Stockholm">Stockholm</option>
-                <option value="Malmö">Malmö</option>
-                <option value="Uppsala">Uppsala</option>
-                <option value="Göteborg">Göteborg</option>
-              </select>
-            </div>
-
-            <div className="flex space-x-4">
-              <div>
-                <label htmlFor="checkInDate" className="block text-sm font-medium text-gray-700">
-                  Incheckning
-                </label>
-                <input
-                  type="date"
-                  id="checkInDate"
-                  value={checkInDate}
-                  onChange={(e) => setCheckInDate(e.target.value)}
-                  className="mt-1 block w-full border-gray-300 text-black rounded-md shadow-sm"
-                />
-              </div>
-              <div>
-                <label htmlFor="checkOutDate" className="block text-sm font-medium text-gray-700">
-                  Utcheckning
-                </label>
-                <input
-                  type="date"
-                  id="checkOutDate"
-                  value={checkOutDate}
-                  onChange={(e) => setCheckOutDate(e.target.value)}
-                  className="mt-1 block w-full border-gray-300 text-black rounded-md shadow-sm"
-                />
-              </div>
-            </div>
-
-            <div>
-              <label htmlFor="adults" className="block text-sm font-medium text-gray-700">
-                ANTAL VUXNA
-              </label>
-              <input
-                type="number"
-                id="adults"
-                value={adults}
-                onChange={(e) => setAdults(e.target.value)}
-                className="mt-1 block w-full border-gray-300 text-black rounded-md shadow-sm"
-                min="1"
-              />
-            </div>
-            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-
-            <button
-              type="submit"
-              className="w-full bg-green-700 text-white py-2 rounded-md hover:bg-green-800 transition"
-            >
-              Sök
-            </button>
-          </form>
-        </div>
-        <div className="ml-10 mr-28">
-          {mainImageUrl && ( // Visa bilden om URL:en har hämtats
-            <Image
-              src={mainImageUrl}
-              alt="Hus"
-              width={700}
-              height={700}
-              className="rounded-md shadow-lg"
-            />
-          )}
-        </div>
-      </section>
-
-      <section className="flex justify-around items-center my-12">
-        <div className="max-w-sm p-6 mb-10">
-          <h2 className="text-2xl font-bold mb-2">Få lite flexibilitet</h2>
-          <p>Boenden med flexibel avbokning gör det enkelt att omboka om dina planer ändras.</p>
-        </div>
-        <div className="max-w-sm p-6">
-          <h2 className="text-2xl font-bold mb-2">Mer än sju miljoner aktiva annonser</h2>
-          <p>Gör som 1 miljard andra gäster som har hittat semesterbostäder i över 220 länder och resmål.</p>
-        </div>
-        <div className="max-w-sm p-6">
-          <h2 className="text-2xl font-bold mb-2">Över 100 filter för skräddarsydda vistelser</h2>
-          <p>Välj prisklass, antal rum och andra viktiga bekvämligheter för att hitta det boende som passar dina behov.</p>
-        </div>
-      </section>
-
-      <section className="container mx-auto my-10 px-4">
-        <h2 className="text-3xl font-bold mb-6 text-center">Boenden att upptäcka</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {boende.map((boende, index) => (
-          <div
-            key={boende.id}
-            className="text-center cursor-pointer "
-            onClick={() => navigateToDetails(boende.id)}
+    <div className="relative">
+  <section className="flex flex-col lg:flex-row justify-between items-center mt-10 lg:mx-24 space-y-10 lg:space-y-0">
+    <div className="bg-white p-4 border border-green-600 rounded-md lg:w-1/2 z-10 lg:mt-0 mt-6 absolute md:relative md:z-auto md:mt-0 top-10 max-w-xs md:max-w-sm 
+    h-auto  md:h-auto lg:h-96 flex flex-col space-y-4"> 
+      <h1 className=" sm:text-2xl font-bold text-black sm:mb-2">Hitta boenden på Airbnb</h1>
+      <p className="text-sm sm:text-base mb-4 text-gray-700">Upptäck hela boenden och rum som är perfekta för alla resor.</p>  
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div>
+          <label htmlFor="location" className="block text-sm font-medium text-gray-700">PLATS</label>
+          <select
+            id="location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            className="mt-1 block w-full border-gray-300 text-black rounded-md shadow-sm text-sm sm:text-base"
           >
-            <Image
-              src={imageUrls[index]}
-              alt={boende.title}
-              width={400}
-              height={300}
-              className="rounded-lg object-cover w-full h-auto"
+            <option value="">Välj stad</option>
+            <option value="Stockholm">Stockholm</option>
+            <option value="Malmö">Malmö</option>
+            <option value="Uppsala">Uppsala</option>
+            <option value="Göteborg">Göteborg</option>
+          </select>
+        </div>
+
+        <div className="flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0">
+          <div>
+            <label htmlFor="checkInDate" className="block text-sm font-medium text-gray-700">Incheckning</label>
+            <input
+              type="date"
+              id="checkInDate"
+              value={checkInDate}
+              onChange={(e) => setCheckInDate(e.target.value)}
+              className="mt-1 block w-full border-gray-300 text-black rounded-md shadow-sm text-sm sm:text-base"
             />
-            <p className="mt-2 text-white">{boende.title}</p>
           </div>
-           ))}
+          <div>
+            <label htmlFor="checkOutDate" className="block text-sm font-medium text-gray-700">Utcheckning</label>
+            <input
+              type="date"
+              id="checkOutDate"
+              value={checkOutDate}
+              onChange={(e) => setCheckOutDate(e.target.value)}
+              className="mt-1 block w-full border-gray-300 text-black rounded-md shadow-sm text-sm sm:text-base"
+            />
           </div>
-      </section>
+        </div>
+
+        <div>
+          <label htmlFor="adults" className="block text-sm font-medium text-gray-700">ANTAL VUXNA</label>
+          <input
+            type="number"
+            id="adults"
+            value={adults}
+            onChange={(e) => setAdults(e.target.value)}
+            className="mt-1 block w-full border-gray-300 text-black rounded-md shadow-sm text-sm sm:text-base"
+            min="1"
+          />
+        </div>
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        <button
+          type="submit"
+          className="w-full bg-green-700 text-white py-2 rounded-md hover:bg-green-800 transition text-sm sm:text-base"
+        >
+          Sök
+        </button>
+      </form>
     </div>
+
+    <div className="hidden lg:block w-full lg:w-1/2">
+      {mainImageUrl && (
+        <Image
+          src={mainImageUrl}
+          alt="Hus"
+          width={700}
+          height={700}
+          className="rounded-md shadow-lg object-cover w-full h-auto"
+        />
+      )}
+    </div>
+  </section>
+
+  <section className="hidden md:flex justify-around items-center my-12">
+    <div className="max-w-sm p-6 mb-10">
+      <h2 className="text-2xl font-bold mb-2">Få lite flexibilitet</h2>
+      <p>Boenden med flexibel avbokning gör det enkelt att omboka om dina planer ändras.</p>
+    </div>
+    <div className="max-w-sm p-6">
+      <h2 className="text-2xl font-bold mb-2">Mer än sju miljoner aktiva annonser</h2>
+      <p>Gör som 1 miljard andra gäster som har hittat semesterbostäder i över 220 länder och resmål.</p>
+    </div>
+    <div className="max-w-sm p-6">
+      <h2 className="text-2xl font-bold mb-2">Över 100 filter för skräddarsydda vistelser</h2>
+      <p>Välj prisklass, antal rum och andra viktiga bekvämligheter för att hitta det boende som passar dina behov.</p>
+    </div>
+  </section>
+
+  <section className="container mx-auto mb-10 px-4">
+    <h2 className=" hidden sm:block text-3xl font-bold mb-6 text-center">Boenden att upptäcka</h2>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 relative">
+      {boende.map((boende, index) => (
+        <div
+          key={boende.id}
+          className="text-center cursor-pointer relative"
+          onClick={() => navigateToDetails(boende.id)}
+        >
+          <Image
+            src={imageUrls[index]}
+            alt={boende.title}
+            width={400}
+            height={300}
+            className="rounded-lg object-cover w-full h-auto"
+          />
+          <p className="mt-3 text-white text-2xl">{boende.title}</p>
+        </div>
+      ))}
+    </div>
+  </section>
+</div>
+
   );
 }
-
 export default Home;
-
-
-
