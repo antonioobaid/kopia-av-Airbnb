a3aa3704690ea1b344772b36b1ea525bddb902a7
Antonio obaid <abo.george11@hotmail.com>
2024-10-29 20:06:18 +0100

    user collection

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,40 +1,57 @@
-"use client"
-import Logo from "./Logo";
-import { SignedIn, SignedOut, UserButton , useClerk } from "@clerk/nextjs";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from "@fortawesome/free-solid-svg-icons"; 
 
+
+"use client";
+import Logo from "./Logo";
+import { SignedIn, SignedOut, UserButton, useClerk, useUser } from "@clerk/nextjs";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+import { doc, setDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../../../firebase/config";
 
 export default function Navbar() {
   const { openSignIn } = useClerk(); 
+  const { user } = useUser(); // Få användarinformation
 
-  // Funktion för att öppna modalen
+  // Uppdatera användarinformation i Firestore vid inloggning
+  if (user) {
+    const updateUserInFirestore = async () => {
+      try {
+        await setDoc(doc(db, "user", user.id), {
+          email: user.primaryEmailAddress?.emailAddress,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          lastLogin: serverTimestamp(),
+        }, { merge: true });
+      } catch (error) {
+        console.error("Error updating user:", error);
+      }
+    };
+
+    updateUserInFirestore();
+  }
+
+  // Funktion för att öppna inloggningsmodalen
   const handleSignInClick = () => {
     openSignIn();
   };
 
   return (
     <div className="flex justify-between items-center p-6 border border-b-2 border-green-600 text-black">
-      
       <div className="hidden sm:block">
         <Logo />
       </div>
 
-      
       <div className="sm:hidden w-full text-center">
         <Logo />
       </div>
 
-     
       <div className="block sm:hidden w-full h-20 bg-gradient-to-r from-[#56953F] to-[#303030] absolute top-0 left-0 z-[-1]"></div>
 
-      <div className="hidden sm:flex space-x-4 ">
-        <SignedIn >
+      <div className="hidden sm:flex space-x-4">
+        <SignedIn>
           <div className="relative flex items-center space-x-4 border bg-green-600 border-black rounded-full px-4 py-1">
-            <FontAwesomeIcon 
-              icon={faBars} 
-              className="w-6 h-6 cursor-pointer text-black" 
-            />
+            <FontAwesomeIcon icon={faBars} className="w-6 h-6 cursor-pointer text-black" />
             <UserButton
               appearance={{
                 elements: {
@@ -47,10 +64,8 @@ export default function Navbar() {
           </div>
         </SignedIn>
 
-      
         <SignedOut>
-          <button onClick={handleSignInClick} className="mx-4 text-xl text-green-600 cursor-pointer hidden sm:block
-          bg-green-100 hover:bg-green-200 rounded-lg px-2 py-1 transition duration-300 ">
+          <button onClick={handleSignInClick} className="mx-4 text-xl text-green-600 cursor-pointer hidden sm:block bg-green-100 hover:bg-green-200 rounded-lg px-2 py-1 transition duration-300">
             Bli medlem
           </button>
         </SignedOut> 
@@ -58,6 +73,7 @@ export default function Navbar() {
     </div>
   );
 }
+
 
 
 
